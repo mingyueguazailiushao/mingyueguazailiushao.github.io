4fe54cb82089689ce0d65ebd53fc5aeaad32de23
haoge <1740500277@qq.com>
2024-03-22 12:13:22 +0800

    网站

---
+++ docs/index.85d594da.js
@@ -0,0 +1,2 @@
+const e=document.getElementsByClassName("picture");for(i=0;i<e.length;i++)e[i].addEventListener("click",function(e){var t;t=e.target,"256px"===window.getComputedStyle(t).height?t.style.setProperty("block-size","768px"):t.style.setProperty("block-size","256px")});
+//# sourceMappingURL=index.85d594da.js.map
